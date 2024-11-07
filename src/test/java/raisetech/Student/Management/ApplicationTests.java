41490edaf61a9b3a08471523a42ff27c48ef9452
s18004kic <s18004@al.kic.ac.jp>
2024-11-07 14:56:04 +0900

    初回コミット

--- src/test/java/raisetech/Student/Management/ApplicationTests.java
+++ src/test/java/raisetech/Student/Management/ApplicationTests.java
@@ -1,13 +1,20 @@
 package raisetech.Student.Management;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class ApplicationTests {
+@SpringBootApplication
+@RestController
+public class ApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
-
+	@GetMapping("/hello")
+	public String hello(){
+		return  "Hello,World";
+	}
 }
